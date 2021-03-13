325b9bf1430f8c33f4b191269a334c9d772d7a86
holypar <singhparminder9411@gmail.com>
2021-03-13 12:24:10 -0800

    figuted out how to embed in discord

--- my_bot.py
+++ my_bot.py
@@ -9,7 +9,21 @@ async def on_ready():
     general_channel = client.get_channel(717414266487177297)
     await general_channel.send("Let's Get these profits!")
 
+@client.event
+async def on_message(message):
+    if message.content == "What is the version?":
+            general_channel = client.get_channel(717414266487177297)
+
+            myEmbed = discord.Embed(title="Current Version", description="The bot is in Version 1.0", color = 0x5db35d)
+            myEmbed.add_field(name = "Version Code", value ="v.1.0.0", inline= False)
+            myEmbed.add_field(name="Date Released:", value ="March 13th,2021", inline= False)
+            myEmbed.set_footer(text="This is the footer")
+            myEmbed.set_author(name ="Par")
+
+
+            await general_channel.send(embed = myEmbed)
+
 #Run the client on the server
-client.run('ODIwMTg3NjcwMjE4NjcwMDgw.YExhSg.4SxJzWvVtBE8Qoxc9vBs676jdn8')
+client.run('ODIwMTg3NjcwMjE4NjcwMDgw.YExhSg.w-nvq4YE6DqWnkLIhhSy5sKX1Dg')
 
 #I made a change
